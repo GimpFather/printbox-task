2c028de30e12aebee7cd0c1fdd39626ac0943aef
GimpFather <filip.galczyk13@gmail.com>
2022-07-30 21:57:03 +0200

    logic and styles

--- src/js/App.js
+++ src/js/App.js
@@ -1,4 +1,4 @@
-class React {
+class Rect {
 	width; //This value should be Number
 	height; //This value should be Number
 	constructor(width, height) {
@@ -7,16 +7,51 @@ class React {
 	}
 }
 
-const tab = [
-	new React(1, 1),
-	new React(2, 2),
-	new React(3, 3),
-	new React(1, 2),
-];
+const tab = [new Rect(1, 1), new Rect(2, 2), new Rect(3, 3), new Rect(1, 2)];
 
-const result = taskLogic(tab, 2);
-console.log(result);
+const main = (rectangles, exportNumber) => {
+	getAllSurfaces(rectangles);
+	calculateAverage(rectangles);
+	getClosestAreas(exportNumber);
+};
 
-// This is the starting code that Printbox gave me.
-// We gonna continue, by doing "taskLogic" function.
-// Currently there should be an error that says "Uncaught ReferenceError: taskLogic is not defined"
+let surfaceArea = 0;
+let closestNumber = 0;
+let averageSurfaceArea = 0;
+let surfacesArray = [];
+
+const getAllSurfaces = (rectangles) => {
+	rectangles.forEach((rectangle) => {
+		let rectangleArea = rectangle.width * rectangle.height;
+		surfaceArea += rectangleArea;
+		surfacesArray.push(rectangleArea);
+	});
+	console.log("List of all areas: " + surfacesArray);
+};
+
+const calculateAverage = (rectangles) => {
+	averageSurfaceArea = surfaceArea = surfaceArea / rectangles.length;
+	console.log("The average surface area: " + averageSurfaceArea);
+};
+
+const getClosestAreas = (exportNumber) => {
+	let finnalArray = [];
+	for (let executions = 0; executions < exportNumber; executions++) {
+		closestNumber = surfacesArray.reduce((prevValue, currValue) => {
+			return Math.abs(currValue - averageSurfaceArea) <
+				Math.abs(prevValue - averageSurfaceArea)
+				? currValue
+				: prevValue;
+		});
+
+		surfacesArray = surfacesArray.filter(
+			(element) => element !== closestNumber
+		);
+		console.log("Thats a winning value: " + closestNumber);
+		finnalArray.push(closestNumber);
+	}
+
+	console.log("Closest numbers: " + finnalArray);
+};
+
+main(tab, 2);
